fddd94a398859a67ebcf86ba00c4f8af4c6c033d
Pierre Berton <pierre.berton@atos.net>
2021-12-05 19:54:25 +0100

    Day 5 completed

---
+++ src/main/java/fr/bertonp/adventofcode/day5/Part1.java
@@ -0,0 +1,85 @@
+package fr.bertonp.adventofcode.day5;
+
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Sets;
+
+import java.io.FileNotFoundException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class Part1 {
+
+    public static void main(String[] args) {
+        try {
+            URL resource = Part1.class.getClassLoader().getResource("day5/input1.txt");
+
+            if (resource == null) {
+                throw new FileNotFoundException();
+            }
+
+            List<String> lines = Files.readAllLines(Paths.get(resource.toURI()), StandardCharsets.UTF_8);
+
+            List<Point> points = new ArrayList<>();
+            for(String line : lines) {
+                String[] lineParts = line.split(Pattern.quote(" -> "));
+                Point p1 = new Point(lineParts[0]);
+                Point p2 = new Point(lineParts[1]);
+
+                List<Point> pointsBetweenTwoPoints = pointsBetweenTwoPoints(p1, p2);
+
+                if(!pointsBetweenTwoPoints.isEmpty()) {
+                    points.add(p1);
+                    points.add(p2);
+                    points.addAll(pointsBetweenTwoPoints);
+                }
+            }
+
+            Map<Point, Integer> countPoints = new HashMap<>();
+
+            for (Point p : points) {
+                countPoints.put(p, countPoints.getOrDefault(p, 0) + 1);
+            }
+
+            int count = (int) countPoints.values().stream().filter(v -> v >= 2).count();
+
+            System.out.println("Result : " + count);
+
+        } catch (Exception e) {
+            System.err.println("Something went wrong :(");
+            e.printStackTrace();
+        }
+    }
+
+    private static List<Point> pointsBetweenTwoPoints(Point p1, Point p2) {
+        List<Point> points = new ArrayList<>();
+        if (p1.getY() == p2.getY()) {
+            if(p1.getX() > p2.getX()) {
+                for (int i = p2.getX() + 1; i < p1.getX(); i++) {
+                    points.add(new Point(i, p1.getY()));
+                }
+            } else {
+                for (int i = p1.getX() + 1; i < p2.getX(); i++) {
+                    points.add(new Point(i, p1.getY()));
+                }
+            }
+        } else if (p1.getX() == p2.getX()) {
+            if(p1.getY() > p2.getY()) {
+                for (int i = p2.getY() + 1; i < p1.getY(); i++) {
+                    points.add(new Point(p1.getX(), i));
+                }
+            } else {
+                for (int i = p1.getY() + 1; i < p2.getY(); i++) {
+                    points.add(new Point(p1.getX(), i));
+                }
+            }
+        }
+
+        return points;
+    }
+
+}
---
+++ src/main/java/fr/bertonp/adventofcode/day5/Point.java
@@ -0,0 +1,50 @@
+package fr.bertonp.adventofcode.day5;
+
+import com.google.common.base.Objects;
+
+import java.util.regex.Pattern;
+
+public class Point {
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point(String s) {
+        String[] coordinates = s.trim().split(Pattern.quote(","));
+        this.x = Integer.parseInt(coordinates[0]);
+        this.y = Integer.parseInt(coordinates[1]);
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Point)) return false;
+        Point point = (Point) o;
+        return x == point.x && y == point.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(x, y);
+    }
+}
