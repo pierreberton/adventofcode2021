9638ab8f2204d87ab6233f5b300d85498f492eb5
Pierre Berton <pierre.berton@atos.net>
2021-12-12 19:45:10 +0100

    Day 12

---
+++ src/main/java/fr/bertonp/adventofcode/day12/Part2.java
@@ -0,0 +1,81 @@
+package fr.bertonp.adventofcode.day12;
+
+import java.io.FileNotFoundException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Part2 {
+
+    static Map<String, Set<String>> caveConnections = new HashMap<>();
+
+    static Integer pathCount = 0;
+
+    public static void main(String[] args) {
+        try {
+            URL resource = Part2.class.getClassLoader().getResource("day12/input1.txt");
+
+            if (resource == null) {
+                throw new FileNotFoundException();
+            }
+
+            List<String> lines = Files.readAllLines(Paths.get(resource.toURI()), StandardCharsets.UTF_8);
+
+            lines.forEach(Part2::createCave);
+            lines.forEach(Part2::createConnections);
+            findPaths("start", "end", new LinkedList<>());
+
+            System.out.println("Result : " + pathCount);
+
+        } catch (Exception e) {
+            System.err.println("Something went wrong :(");
+            e.printStackTrace();
+        }
+    }
+
+    private static void findPaths(String from, String to, List<String> visited) {
+        visited.add(from);
+        if (from.equals(to)) {
+            pathCount++;
+            return;
+        }
+
+        for (var conn : caveConnections.get(from)) {
+            if (canVisit(conn, visited)) {
+                List<String> currentPath = new LinkedList<>(visited);
+                findPaths(conn, to, visited);
+                visited = new LinkedList<>(currentPath);
+            }
+        }
+    }
+
+    private static boolean canVisit(String conn, List<String> visited) {
+        if (conn.equals("start")) {
+            return false;
+        }
+
+        if (conn.matches("^[A-Z]+$") || !visited.contains(conn)) {
+            return true;
+        }
+
+        List<String> smallsVisited = visited.stream().filter(cave -> cave.matches("^[a-z]+$")).collect(Collectors.toList());
+        List<String> distinctSmallsVisited = smallsVisited.stream().distinct().collect(Collectors.toList());
+        return (smallsVisited.size() == distinctSmallsVisited.size());
+    }
+
+    private static void createConnections(String line) {
+        List<String> caves = Arrays.stream(line.split("-")).collect(Collectors.toList());
+        caveConnections.get(caves.get(0)).add(caves.get(1));
+        caveConnections.get(caves.get(1)).add(caves.get(0));
+    }
+
+    static void createCave(String line) {
+        List<String> caves = Arrays.stream(line.split("-")).collect(Collectors.toList());
+        caveConnections.put(caves.get(0), new HashSet<>(Set.of(caves.get(1))));
+        caveConnections.put(caves.get(1), new HashSet<>(Set.of(caves.get(0))));
+    }
+
+}
